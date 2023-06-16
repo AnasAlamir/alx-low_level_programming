c931c4146294b7b01a1ab7d4bd65cf5d8ab339db
AnasAlamir <anasalamir06@gmail.com>
2023-06-16 08:57:59 -0700

    new files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c program to print using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-     puts("Programming is like building a multilingual puzzle\n");
-     return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c program to print using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
