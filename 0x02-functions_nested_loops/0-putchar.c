05c9d8d749bc9ca00ba1d2eac5e4b2681e702261
AnasAlamir <anasalamir06@gmail.com>
2023-06-22 06:41:48 -0700

    new files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(i);
+	_putchar(\n);
 	return (0);
 }
