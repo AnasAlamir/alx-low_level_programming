f823360ec9f5fb37e8ea92030b4d5a3292ddc886
AnasAlamir <anasalamir06@gmail.com>
2023-06-25 02:53:04 -0700

    11. 98 Battery Street, the OG file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_to_98 -  function that prints all natural numbers from n to 98.
+ *
+ * @n: input
+ *
+ * Return: res
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 9)
+				_putchar('0' + i);
+			else
+			{
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+			}
+			if (i == 98)
+			{
+				_putchar('\n');
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		}
+	else
+	{
+		for (i = n; i <= 98; i--)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			if (i == 98)
+			{
+				_putchar('\n');
+				break;
+			}
+			 _putchar(',');
+			_putchar(' ');
+		}
+	}
+}
