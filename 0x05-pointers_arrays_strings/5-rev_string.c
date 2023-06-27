7e73bf055e37037785037b44a875462fb141447a
AnasAlamir <anasalamir06@gmail.com>
2023-06-26 17:43:23 -0700

    not completed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: input fun
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char *p;
+
+	while (*s != '\n')
+		i++;
+	while (i--)
+		p[i] = s[i];
+	s = p;
+}
