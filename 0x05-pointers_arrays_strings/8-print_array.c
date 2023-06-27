a87fa4770e5418f487940746cde63403f885f510
AnasAlamir <anasalamir06@gmail.com>
2023-06-27 06:49:19 -0700

    new files

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
+	{
+		if(i == n - 1)
+		{
+			printf("%d", a[n - 1]);
+			break;
+		}
 		printf("%d, ", a[i]);
-	printf("%d", a[n - 1]);
+	}
 	printf("\n");
 }
