2b4774c51d004012440bbfabedc507fbab258b56
AnasAlamir <anasalamir06@gmail.com>
2023-06-30 14:47:22 -0700

    new files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[l])
 		l++;
-	for (i = 0; n && src[i]; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[l + i] = src[i];
 	dest[l + i] = '\0';
 	return (dest);
