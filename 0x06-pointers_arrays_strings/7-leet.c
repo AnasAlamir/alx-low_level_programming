b8d340929537c2c2898a9777a93a21519b8eada5
AnasAlamir <anasalamir06@gmail.com>
2023-07-01 08:02:49 -0700

    last new files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,8 +7,16 @@
  *
  * Return: a
  */
-char *leet(char a)
+char *leet(char *a)
 {
+	int i, j;
+	char b[] ="4A4a3E3e0O0o7T7t1L1l";
 
+	for (i = 0; a[i]; i++)
+	{
+		for (j = 0; b[j]; j++)
+			if (a[i] == b[j] && j % 2 != 0)
+				a[i] = b[j - 1];
+	}
 	return (a);
 }
