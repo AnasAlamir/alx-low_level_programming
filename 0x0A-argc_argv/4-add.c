1b7de138b06dd24b95817c8027985522019af999
AnasAlamir <anasalamir06@gmail.com>
2023-07-10 06:14:34 -0700

    4. To infinity and beyond edit 2 on betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	else
 	{
-		for (i = 1 ; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (argv[i][0] >= 48 && argv[i][0] <= '9')
 				r += atoi(argv[i]);
