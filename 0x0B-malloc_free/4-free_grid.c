9f1bcdc2cc509777d27817e3da5937ab7ba7cd5a
AnasAlamir <anasalamir06@gmail.com>
2023-07-16 13:58:39 -0700

    new edited files

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * free_grid -  that frees a 2 dimensional grid previously
+ *
+ * @grid: input
+ *
+ * @height:input
+ *
+ * Return: NULL if size = 0
+ *              otherwise a pointer, or NULL if it fails
+ */
+
+void free_grid(int **grid, int height)
+{
+	int h;
+
+	h = 0;
+	while (h <= height)
+	{
+		free(grid[h]);
+		h++;
+	}
+	free(grid);
+}
