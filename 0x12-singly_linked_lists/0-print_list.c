efa35fcf4c418830d82edf6948bf546f32147237
AnasAlamir <anasalamir06@gmail.com>
2023-07-30 15:25:46 -0700

    0-print_list.c6

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,10 +16,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%ld] %s\n", strlen(h->str), h->str);
 		ie++;
 		h = h->next;
 	}
-	printf("-> %lu elements\n", ie);
 	return (ie);
 }
