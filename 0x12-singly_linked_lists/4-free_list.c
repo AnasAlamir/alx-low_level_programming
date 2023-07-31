14adcdcc1b3bed5adb11f7b6792fd2c5ad4e4f1e
AnasAlamir <anasalamir06@gmail.com>
2023-07-31 05:58:24 -0700

    4-free_list.c2

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,15 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current = head;
+	list_t *current , *next;
 	if (head == NULL)
 		return;
-	while (current->next)
+	current = head;
+	while (current)
 	{
+		next = current->next;
+		free(current->str);
 		free(current);
-		current = current->next;
+		current = next;
 	}
 }
