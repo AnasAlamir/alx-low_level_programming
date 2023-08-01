0db410e606871a4ee092ca7ce2049620f9a7f746
AnasAlamir <anasalamir06@gmail.com>
2023-08-01 10:29:59 -0700

    1-listint_len.c2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len -  that returns the number of elements in a linked listint_t list.
+ * listint_len -  that returns the number of elements.
  *
  * @h: input
  *
