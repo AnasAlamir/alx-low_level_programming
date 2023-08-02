24a1dd5342127e60edede7610134e70d027a825a
AnasAlamir <anasalamir06@gmail.com>
2023-08-02 04:10:02 -0700

    10_3

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ind = *head;
 		*head = ind->next;
 		free(ind);
-		return (-1);
+		return (1);
 	}
 	ind = *head;
 	while (i < index && ind)
